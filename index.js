32b0adc610627f4013d51956f2bcee9a9dacff39
Shivam <shivamnegi478@gmail.com>
2023-10-18 02:19:57 +0530

    sockets basic

---
+++ index.js
@@ -0,0 +1,33 @@
+const { log } = require('console');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use('/', express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    
+    /* socket.on('from_client', () => {
+        console.log("received from client");
+    })
+
+    setInterval(() => {
+        socket.emit('from_server');
+    }, 3000); */
+    socket.on('new_msg', (data) => {
+        // io.emit('msg_rcvd', data);
+        // socket.emit('msg_rcvd', data);
+        socket.broadcast.emit('msg_rcvd', data);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+}); 
---
+++ public/script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('welcome to sockets');
+    var socket = io();
+
+    /* let btn = document.getElementById("btn");
+    btn.addEventListener("click", () => {
+        socket.emit("from_client");
+    })
+
+    socket.on('from_server', () => {
+        let div = document.getElementById('from_server');
+        let p = document.createElement('p');
+        p.textContent = 'received an event from the server';
+        div.appendChild(p);
+    }) */
+
+    let input = document.getElementById("chat_box");
+    let msgList = document.getElementById("msg_list");
+    let send = document.getElementById("send");
+
+    send.addEventListener("click", () => {
+        let msg = input.value;
+        console.log(msg);
+        socket.emit('new_msg', {
+            message: msg
+        });
+        input.value = "";
+    });
+    socket.on('msg_rcvd', (data) => {
+        console.log('data', data);
+        let msg = document.createElement("li");
+        msg.textContent= data.message;
+        msgList.appendChild(msg);
+    })
+})
